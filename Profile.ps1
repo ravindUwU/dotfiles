e4c8c5c23493e2897ecff5ac6cb9e695c30abcc2
Ravindu Liyanapathirana <Ravindu.Liyanapathirana@monash.edu>
2023-01-04 11:44:08 +1100

    add windows terminal helpers

--- Profile.ps1
+++ Profile.ps1
@@ -32,6 +32,36 @@ function Reload-Path
 		+ [System.Environment]::GetEnvironmentVariable('PATH','User')
 }
 
+# Windows Terminal
+# TODO: Make conditional on OS == Windows, maybe also Windows Terminal installed?
+# TODO: Document!
+# TODO: `function wt` with better documentation, etc.
+# TODO: Is there a better way to accept $dir? DirectoryInfo or FileInfo (or equilvant) objects
+#       instead of string?
+
+function wt_NewTab
+{
+	param([string] $path = '.')
+
+	wt.exe --window 0 new-tab --startingDirectory (Resolve-Path $path).Path
+}
+
+function wt_Split
+{
+	[CmdletBinding()]
+	param(
+		[Parameter(Mandatory)] [ValidateSet('v','h')] [string] $direction,
+		[Parameter()] [string] $path = '.'
+	)
+
+	switch ($direction) {
+		'v' { $directionArg = '--vertical' }
+		'h' { $directionArg = '--horizontal' }
+	}
+
+	wt.exe --window 0 split-pane $directionArg --startingDirectory (Resolve-Path $path).Path
+}
+
 # Aliases
 Set-Alias gh Get-Help
 Set-Alias x explorer.exe
