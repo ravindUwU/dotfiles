4a54e3b28a9f1e611b995ae75f845fd799cc6fd3
Ravindu Liyanapathirana <Ravindu.Liyanapathirana@monash.edu>
2024-04-16 16:21:58 +1000

    Add cdp.ps1

---
+++ pwsh/include/cdp.ps1
@@ -0,0 +1,52 @@
+<#
+.SYNOPSIS
+	Open a project located in the ~/Projects directory.
+
+.NOTES
+	Use ~/Dotfiles.Cdp.Projects.ps1 to configure subprojects,
+
+		@{
+			'<project>' = @{
+				Subprojects = ('<subproject 1>', '<subproject 2>')
+			}
+		}
+#>
+function cdp {
+	# Load config
+	if (Test-Path "$HOME/Dotfiles.Cdp.Projects.ps1") {
+		$config = . "$HOME/Dotfiles.Cdp.Projects.ps1"
+	}
+
+	# Make list of projects
+	$projects = Get-ChildItem "$HOME/Projects" -Directory | ForEach-Object {
+
+		# Yield project
+		$projectName = $_.Name
+		$projectPath = $_.FullName
+		[PSCustomObject]@{
+			Name = $projectName
+			Path = $projectPath
+		}
+
+		# Yield subprojects
+		($config[$_.Name].Subprojects ?? @()) | ForEach-Object {
+			$subprojectName = "$projectName/$_"
+			$subprojectPath = Join-Path $projectPath $_
+			[PSCustomObject]@{
+				Name = $subprojectName
+				Path = $subprojectPath
+			}
+		}
+	}
+
+	# Prompt for selection using fzf
+	$selection = $projects | ForEach-Object { $_.Name } | fzf --height '30%' --layout reverse
+
+	# Find selected project & cd into it
+	$project = $projects | Where-Object Name -eq $selection | Select-Object -First 1
+	if ($null -ne $project) {
+		Set-Location $project.Path
+	}
+}
+
+Invoke-Command { Export-DotfilesFunction 'cdp' } 2>&1 | Out-Null
