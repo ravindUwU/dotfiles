0543ab0127e137c4647be2a6bf849eb29e949e77
Ravindu Liyanapathirana <Ravindu.Liyanapathirana@monash.edu>
2024-04-16 14:36:26 +1000

    List processes that are stopped in ahk/Stop.ps1

--- ahk/Stop.ps1
+++ ahk/Stop.ps1
@@ -1,3 +1,9 @@
 Get-Process 'AutoHotkey64' -ErrorAction SilentlyContinue `
-	| Where-Object { $_.CommandLine.ToLower().Contains($PSScriptRoot.ToLower()) } `
-	| Stop-Process
+	| Where-Object {
+		# CLI includes path to a Dotfiles AHK script
+		$_.CommandLine -like "*$PSScriptRoot[/\]*.ahk*"
+	} `
+	| ForEach-Object {
+		Write-Output "Stopping $($_.Name)#$($_.Id) ($($_.CommandLine))"
+		$_ | Stop-Process
+	}
