f3a595bc17dfa8c241b30be8e7d948142fe6cd09
Ravindu Liyanapathirana <Ravindu.Liyanapathirana@monash.edu>
2024-05-01 17:32:49 +1000

    Make cdv.ps1 cd into containing directory if clipboard path is to a file

--- pwsh/include/cdv.ps1
+++ pwsh/include/cdv.ps1
@@ -1,5 +1,11 @@
 function cdv {
-	Set-Location (Get-Clipboard)
+	$path = (Resolve-Path (Get-Clipboard)).Path
+
+	if (Test-Path $path -PathType Leaf) {
+		$path = Join-Path $path '..' -Resolve
+	}
+
+	Set-Location $path
 }
 
 Invoke-Command { Export-DotfilesFunction 'cdv' } 2>&1 | Out-Null
