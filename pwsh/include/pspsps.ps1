68cb7aad9c7d351884f2ca88dbabe873744b6269
Ravindu Liyanapathirana <Ravindu.Liyanapathirana@monash.edu>
2024-12-31 19:04:53 +1100

    Add pspsps.ps1 :3

---
+++ pwsh/include/pspsps.ps1
@@ -0,0 +1,18 @@
+<#
+.SYNOPSIS
+	:3
+
+.LINK
+	https://bsky.app/profile/did:plc:dq6jaefxy6vc6vvthg57kwvs/post/3lc7i7njzcs22
+#>
+function pspsps {
+@'
+        z ᶻ
+         z
+,,-------,^^.
+```
+'@
+}
+
+& ((Get-Command 'Export-DotfilesFunction' -ErrorAction Ignore) ?? {}) 'pspsps'
+
