4fb2e6eb9bdeb9c6c98cb82bcb29dbc23de49061
Ravindu Liyanapathirana <Ravindu.Liyanapathirana@monash.edu>
2025-01-28 15:21:50 +1100

    Alias sl to pspsps
    I keep accidentally typing sl instead of ls, which cd-s into the home directory when no path is
    specified. This makes sl show an eeper instead

--- pwsh/include/aliases.ps1
+++ pwsh/include/aliases.ps1
@@ -2,10 +2,11 @@ function Set-DotfilesAliases {
 
 	$aliases = @{
 		'gh' = 'Get-Help'
+		'sl' = 'pspsps'
 	}
 
 	foreach ($a in $aliases.GetEnumerator()) {
-		Set-Alias -Name $a.Key -Value $a.Value -Scope 'Global'
+		Set-Alias -Name $a.Key -Value $a.Value -Scope 'Global' -Force
 	}
 
 	$aliases.Keys
