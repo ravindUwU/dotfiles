09bb0961a4f5287305dcb42045efcb867129c0bb
Ravindu Liyanapathirana <Ravindu.Liyanapathirana@monash.edu>
2025-03-22 18:14:03 +1100

    Remove gcbl.ps1
    Didn't end up using this... (gcb).Length is easy enough to type.

--- pwsh/include/gcbl.ps1
+++
@@ -1,9 +0,0 @@
-<#
-.SYNOPSIS
-	Gets the length of the contents of the clipboard.
-#>
-function gcbl {
-	(Get-Clipboard).Length
-}
-
-& ((Get-Command 'Export-DotfilesFunction' -ErrorAction Ignore) ?? {}) 'gcbl'
