dbc8b09300bdf6e00b3ea5568f21f71ee1b82973
Ravindu Liyanapathirana <ravinduwu@outlook.com>
2026-03-20 14:09:01 +1100

    Add pwsh autocd
    Bash has a feature called autocd which allows navigating to directories by specifying their name,
    without cd in front!
    https://www.gnu.org/software/bash/manual/html_node/The-Shopt-Builtin.html#:~:text=autocd
    
    Pwsh has a "command not found" handler https://gist.github.com/Jaykul/b8ed295d32ec2500b7becfed38308521
    and I've used this to make autocd-like behaviour activated when the command begins with ./ or ../,
    so "./huh/" to navigate to that directory would work, but simply "huh" wouldn't.

--- pwsh/Dotfiles.psm1
+++ pwsh/Dotfiles.psm1
@@ -1,6 +1,7 @@
 param (
 	# May include,
 	#     [bool] UsePrompt
+	#     [bool] UseAutoCd
 	[hashtable] $Options
 )
 
@@ -25,19 +26,17 @@ Export-ModuleMember `
 	-Function ${script:exportedFunctions} `
 	-Alias (Set-DotfilesAliases)
 
-# Install prompt if requested
-if ($Options.UsePrompt) {
-	Install-DotfilesPrompt
-}
+# Install requested features
+if ($Options.UsePrompt) { Install-DotfilesPrompt }
+if ($Options.UseAutoCd) { Install-DotfilesAutoCd }
 
 # Clean up if the module is removed
 # https://learn.microsoft.com/powershell/module/microsoft.powershell.core/remove-module?view=powershell-7.4#example-5-using-the-onremove-event
 $ExecutionContext.SessionState.Module.OnRemove += {
 
-	# Uninstall prompt if previously installed
-	if ($Options.UsePrompt) {
-		Uninstall-DotfilesPrompt
-	}
+	# Uninstall previously installed features
+	if ($Options.UsePrompt) { Uninstall-DotfilesPrompt }
+	if ($Options.UseAutoCd) { Uninstall-DotfilesAutoCd }
 
 	# Remove Dotfiles variables
 	Remove-Variable -Name 'Dotfiles.*' -Scope 'Global'
---
+++ pwsh/include/autocd.ps1
@@ -0,0 +1,38 @@
+function Install-DotfilesAutoCd {
+	[CmdletBinding()]
+	param ()
+
+	${global:DotFiles.AutoCd.Old} = $ExecutionContext.SessionState.InvokeCommand.CommandNotFoundAction
+
+	# https://github.com/PowerShell/PowerShell/blob/a51e7be624ab6dbde17f1d35ef470b69b8ebc2db/src/System.Management.Automation/engine/MshCmdlet.cs#L340-L347
+	$ExecutionContext.SessionState.InvokeCommand.CommandNotFoundAction = {
+		param ([object] $s, [System.Management.Automation.CommandLookupEventArgs] $e)
+
+		if (
+			(
+				# Command starts with ./ or ../
+				$e.CommandName.StartsWith(".$([System.IO.Path]::DirectorySeparatorChar)") `
+				-or $e.CommandName.StartsWith(".$([System.IO.Path]::AltDirectorySeparatorChar)") `
+				-or $e.CommandName.StartsWith("..$([System.IO.Path]::DirectorySeparatorChar)") `
+				-or $e.CommandName.StartsWith("..$([System.IO.Path]::AltDirectorySeparatorChar)")
+			) `
+			-and (
+				# Container directory exists
+				Test-Path -PathType Container $e.CommandName
+			)
+		) {
+			$e.CommandScriptBlock = { cd $e.CommandName }.GetNewClosure()
+			$e.StopSearch = $true
+		}
+	}
+}
+
+function Uninstall-DotfilesAutoCd {
+	[CmdletBinding()]
+	param ()
+
+	if (${global:DotFiles.AutoCd.Old}) {
+		$ExecutionContext.SessionState.InvokeCommand.CommandNotFoundAction = ${global:DotFiles.AutoCd.Old}
+		Remove-Variable -Name 'DotFiles.AutoCd.Old' -Scope 'Global'
+	}
+}
