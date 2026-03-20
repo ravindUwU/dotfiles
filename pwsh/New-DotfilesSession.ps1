661c4fc9c165c1045fdab0b059f265ccf3e24be3
Ravindu Liyanapathirana <ravinduwu@outlook.com>
2026-03-20 14:39:26 +1100

    Pass init script block directly to pwsh in New-DotfilesSession, without attempting to stringify it
    When a script block is passed to a native command, pwsh converts it to the following CLI args:
        -encodedCommand ... -inputFormat xml -outputFormat text
    
    This can be observed:
        PS> cmd /C 'ECHO' { 'woag!' }
        -encodedCommand IAAnAHcAbwBhAGcAIQAnACAA -inputFormat xml -outputFormat xml

--- pwsh/New-DotfilesSession.ps1
+++ pwsh/New-DotfilesSession.ps1
@@ -3,52 +3,31 @@
 	Starts a PowerShell instance with the Dotfiles module loaded. Intended for use when testing out
 	changes to contents of the module.
 #>
-param (
-	[Parameter()]
-	[switch] $Confirm
-)
+[CmdletBinding()]
+param ()
 
-$init = {
-	# Override prompt label
-	${global:Dotfiles.Prompt.Label} = 'DOTFILES'
+try {
+	Push-Location '.'
+	Set-Location $PSScriptRoot
 
-	# Import module
-	Import-Module '.\Dotfiles.psm1' -ArgumentList @{ 'UsePrompt' = $true ; 'UseAutoCd' = $true }
+	# Start pwsh
+	pwsh -NoLogo -NoProfile -NoExit -WorkingDirectory $PSScriptRoot {
+		# Override prompt label
+		${global:Dotfiles.Prompt.Label} = 'DOTFILES'
 
-	# Exit immediately, if the module didn't load
-	if (-not (Get-Module 'Dotfiles' -ErrorAction Ignore)) {
-		Write-Host 'Module not loaded; exiting' -ForegroundColor Red
-		exit
+		# Import module
+		Import-Module './Dotfiles.psm1' -ArgumentList @{
+			'UsePrompt' = $true
+			'UseAutoCd' = $true
+		}
+
+		# Exit immediately, if the module didn't load
+		if (-not (Get-Module 'Dotfiles' -ErrorAction Ignore)) {
+			Write-Host 'Module not loaded; exiting' -ForegroundColor Red
+			exit
+		}
 	}
 }
-
-# Try to convert the init script into a single command string. Syntax unaware string manipulation
-# that removes indentation, empty lines and line comments.
-# TODO: Convert init script into single line command using the PowerShell parser/script block AST?
-$commands = `
-	($init.ToString() -replace '\t','').Trim() `
-	-replace '\r','' `
-	-split '\n' `
-	| Where-Object { $_.Trim()  } `
-	| Where-Object { -not $_.StartsWith('#') }
-
-$command = $commands -join ' ; '
-
-# Write out commands & confirm if requested
-if ($Confirm) {
-	Write-Host 'Script:' -ForegroundColor Cyan
-	$commands | ForEach-Object { Write-Host "`t $_" }
-
-	Write-Host "`nCommand:" -ForegroundColor Cyan
-	Write-Host "`t$command`n"
-
-	if ($Host.UI.PromptForChoice($null, 'Continue?', @(
-		[System.Management.Automation.Host.ChoiceDescription]::new('&No')
-		[System.Management.Automation.Host.ChoiceDescription]::new('&Yes')
-	), 0) -ne 1) {
-		exit
-	}
+finally {
+	Pop-Location
 }
-
-# Start instance
-pwsh -NoLogo -NoProfile -NoExit -WorkingDirectory $PSScriptRoot -Command $command
